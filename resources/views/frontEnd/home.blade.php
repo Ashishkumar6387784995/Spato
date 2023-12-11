51c516bf5bb4f3f693626f0e310bba189e6e30ca
bhaskar <bhaskarpratap111@gmail.com>
2023-12-11 17:06:56 +0530

    ss

--- resources/views/frontEnd/home.blade.php
+++ resources/views/frontEnd/home.blade.php
@@ -1004,40 +1004,7 @@
     });
   </script>
   <!-- Darkmode script starts -->
-  <script>
-      document.addEventListener('DOMContentLoaded', function () {
-        const darkModeToggle = document.getElementById('darkModeToggle');
-    
-        darkModeToggle.addEventListener('change', function () {
-          document.body.classList.toggle('dark-mode', darkModeToggle.checked);
-          toggleDarkModeElements(darkModeToggle.checked);
-        });
-    
-        // Function to toggle dark mode for specific elements with different classes and tags
-        function toggleDarkModeElements(isDarkMode) {
-          const elementsToToggle = document.querySelectorAll('.dark-view');
-    
-          elementsToToggle.forEach(function (element) {
-            element.classList.toggle('dark-mode', isDarkMode);
-          });
-        }
-        
-      });
-      function myFunction() {
-    const myDiv = document.getElementById('hero');
-
-    // Check the current background image
-    const currentImage = myDiv.style.backgroundImage;
-
-    // Toggle between two different background images
-    if (currentImage.includes('assets/frontEnd/web/images/remove.png')) {
-      myDiv.style.backgroundImage = "url({{ asset('assets/frontEnd/web/images/dark-bg.png')}} )";
-    } else {
-      myDiv.style.backgroundImage = "url({{ asset('assets/frontEnd/web/images/dark-bg.png')}} )";
-    }
-    
-  }
-    </script>
+  <script type="text/javascript" src="{{ asset('/js/web/dark_theme.js') }}"></script>
   <!-- Darkmode script ends -->
 
 </body>
