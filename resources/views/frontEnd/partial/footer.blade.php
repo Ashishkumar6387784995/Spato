f08ad05802f77aacd4fa8a617653db0326ad77ea
darksitecoder <sumitpal4689@gmail.com>
2023-12-13 11:13:35 +0530

    vendor login

--- resources/views/frontEnd/partial/footer.blade.php
+++ resources/views/frontEnd/partial/footer.blade.php
@@ -39,7 +39,7 @@
           <p class="footer-desc"><a href="#">Verrohrung</a></p>
         </div>
         <div class="col">
-          <p class="footer-desc"><a href="#">Vendor Login</a></p>
+          <p class="footer-desc" data-bs-toggle="modal" data-bs-target="#exampleModal"><a href="#">Vendor Login</a></p>
           <p class="footer-desc"><a href="{{url('api/contact')}}">Kontakt</a></p>
           <p class="footer-desc"><a href="{{url('api/cancelationPolicy')}}">Cancellation</a></p>
           <p class="footer-desc" data-bs-toggle="modal" data-bs-target="#exampleModal"><a href="#">Login</a></p>
