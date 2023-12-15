559172b16f680bf56fe0c1312a80f0e8f43b5460
bhaskar <bhaskarpratap111@gmail.com>
2023-12-15 17:36:06 +0530

    sdc

--- resources/views/frontEnd/Pages/poolBuilder/pollProfile.blade.php
+++ resources/views/frontEnd/Pages/poolBuilder/pollProfile.blade.php
@@ -10,10 +10,44 @@
  @include('frontEnd/partial/header')
  <!-- Header -->
 
+ <div style="padding:200px"><b>Profile Settings</b><br><br>
+
+ <input type="" style="border-radius:100px; height:100px; width:100px" placeholder="edit"/>
+ <span>Alex</span>
+ <span>ExampleAddress@gmail.com</span>
+
+ <div>Refrence</div>
+
+ <div><Select style="width:300px">
+    <option>dfgkldshghsdk</option>
+    <option>dgdfgdfgsdfgd</option>
+    <option>dgdfgdfgsdfgd</option>
+    <option>dgdfgdfgsdfgd</option>
+    <option>dgdfgdfgsdfgd</option>
+    <option>dgdfgdfgsdfgd</option>
+ </Select></div>
+
+
+ <div>Upload Previous Work Images</div>
+
+<div><button class="btn btn-danger">Upload</button></div>
+<br><br>
+<div>Setting</div>
+<br>
+<div><button class="btn btn-danger">Active</button> <button class="btn btn-danger">Deactive</button></div>
+
+</div>
+
+
+
+
+
+
+
 
 
  <section class="registration">
-  Hello
+
  </section>
 <!-- Footer -->
 @include('frontEnd/partial/footer')
