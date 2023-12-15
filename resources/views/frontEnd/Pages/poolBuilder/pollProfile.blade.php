b07ecfa061d110107389196fac66ba2ff2fc3314
darksitecoder <sumitpal4689@gmail.com>
2023-12-15 16:58:27 +0530

    sdea

---
+++ resources/views/frontEnd/Pages/poolBuilder/pollProfile.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <!-- Header -->
+ @include('frontEnd/partial/header')
+ <!-- Header -->
+
+
+
+ <section class="registration">
+  Hello
+ </section>
+<!-- Footer -->
+@include('frontEnd/partial/footer')
+<!-- Footer -->
+</body>
+</html>
