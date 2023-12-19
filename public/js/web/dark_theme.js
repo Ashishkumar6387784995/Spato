6b58ed06b55f671dd3d0a7fb7c169ebfb9a599e2
darksitecoder <sumitpal4689@gmail.com>
2023-12-19 11:05:33 +0530

    sdcfvgbnm

--- public/js/web/dark_theme.js
+++ public/js/web/dark_theme.js
@@ -1,50 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const darkModeToggle = document.getElementById("darkModeToggle");
-
-    darkModeToggle.addEventListener("change", function () {
-        document.body.classList.toggle("dark-mode", darkModeToggle.checked);
-        toggleDarkModeElements(darkModeToggle.checked);
-    });
-
-    // Function to toggle dark mode for specific elements with different classes and tags
-    function toggleDarkModeElements(isDarkMode) {
-        const elementsToToggle = document.querySelectorAll(".dark-view");
-
-        elementsToToggle.forEach(function (element) {
-            element.classList.toggle("dark-mode", isDarkMode);
-        });
-    }
-});
-
-// function myFunction() {
-//     const myDiv = document.getElementById("hero");
-
-//     // Check the current background image
-//     const currentImage = myDiv.style.backgroundImage;
-
-//     // Toggle between two different background images
-//     if (currentImage.includes("assets/frontEnd/web/images/light-bg.png")) {
-//         myDiv.style.backgroundImage =
-//             "url(http://127.0.0.1:8000/assets/frontEnd/web/images/dark-background.png)";
-//     } else {
-//         myDiv.style.backgroundImage =
-//             "url(http://127.0.0.1:8000/assets/frontEnd/web/images/light-bg.png)";
-//     }
-// }
-
 function toggleDarkMode() {
     const body = document.body;
-    body.classList.toggle("dark-mode");
+    body.classList.toggle("body-dark");
+
+    const darkModeSections = document.querySelectorAll(".dark-mode-section1");
+
+    // Toggle dark mode styles for specific sections
+    darkModeSections.forEach((section) => {
+        section.classList.toggle("dark-mode-background1");
+    });
+
+    const darkModeotherSections = document.querySelectorAll(
+        ".dark-mode-section2"
+    );
+
+    // Toggle dark mode styles for specific sections
+    darkModeotherSections.forEach((section) => {
+        section.classList.toggle("dark-mode-background2");
+    });
 
     const image = document.getElementById("myImage");
-    const currentSrc = image.src;
+    // const currentSrc = image.src;
 
     // Replace image source based on dark mode
-    if (body.classList.contains("dark-mode")) {
+    if (body.classList.contains("body-dark")) {
         image.src =
-            "{{ asset('assets/frontEnd/web/images/dark-background.png')}}";
+            "http://127.0.0.1:8000/assets/frontEnd/web/images/dark-background.png";
     } else {
         image.src =
-            "{{ asset('assets/frontEnd/web/images/light-background.png')}}";
+            "http://127.0.0.1:8000/assets/frontEnd/web/images/light-background.png";
     }
 }
