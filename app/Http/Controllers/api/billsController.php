aba23103075c8a268c8082f997eea83b2d76c65e
bhaskar <bhaskarpratap111@gmail.com>
2023-12-20 12:57:32 +0530

    dd

--- app/Http/Controllers/api/billsController.php
+++ app/Http/Controllers/api/billsController.php
@@ -15,11 +15,11 @@ class billsController extends Controller
 
     public function editAssignment(){
      
-        return view('admin_theme/pages/assignments/editAssignments');
+        return view('admin_theme/pages/bills/editbills');
     }
 
     public function addAssignment(){
      
-        return view('admin_theme/pages/assignments/addAssignments');
+        return view('admin_theme/pages/bills/addbills');
     }
 }
