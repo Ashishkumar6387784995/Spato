92c44962786c9e74ffed2d5bfcf46777b80b4a3d
bhaskar <bhaskarpratap111@gmail.com>
2023-12-20 14:50:21 +0530

    dd

--- app/Http/Controllers/api/claims_manager_Controller.php
+++ app/Http/Controllers/api/claims_manager_Controller.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class claims_manager_Controller extends Controller
 {
-    //
+    public function billsListing(){
+     
+        return view('admin_theme/pages/bills/assignmentList');
+    }
+
+
+    public function editbills(){
+     
+        return view('admin_theme/pages/bills/editbills');
+    }
+
+    public function addbills(){
+     
+        return view('admin_theme/pages/bills/addbills');
+    }
 }
