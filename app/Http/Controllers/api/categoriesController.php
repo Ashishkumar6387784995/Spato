bae6cb523451823d16a649081fffca092db5163a
bhaskar <bhaskarpratap111@gmail.com>
2024-01-12 12:27:49 +0530

    dd

--- app/Http/Controllers/api/categoriesController.php
+++ app/Http/Controllers/api/categoriesController.php
@@ -70,7 +70,7 @@ class categoriesController extends Controller
         $validator = Validator::make($request->all(), [
             'Kategorie_Nr' => 'required|string',
             'Kategorie_datum' => 'nullable|date',
-            'Kategorie_Name' => 'required|string',
+            'Kategorie_Name' => 'required|string|max:50',
             'Kategorie_Beschreibung' => 'nullable|string',
             'imageFile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
