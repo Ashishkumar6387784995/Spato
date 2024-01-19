791f249167d6285a5047030ca1834ca5001ab6ae
darksitecoder <sumitpal4689@gmail.com>
2024-01-19 17:45:37 +0530

    ss

--- resources/views/frontEnd/Pages/setting.blade.php
+++ resources/views/frontEnd/Pages/setting.blade.php
@@ -11,9 +11,9 @@
    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
   <link rel="stylesheet" href="{{ asset('style/web/home.css') }}">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
   <script src="https://kit.fontawesome.com/e1528f4468.js" crossorigin="anonymous"></script>
 
