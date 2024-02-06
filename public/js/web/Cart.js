36b912c0a6eb2093592c85cf0097ac1ac267053f
Ashish <ashish.kumar@nettechnocrats.com>
2024-02-06 12:43:10 +0530

    calculateGrandTotal

--- public/js/web/Cart.js
+++ public/js/web/Cart.js
@@ -1,10 +1,12 @@
-$(document).ready(function () {
+
+$(document).ready(function() {
     updateCartItemsList();
 });
 
+
 // Example function to decrease quantity
 function decreaseQuantity(productId) {
-    var quantityInput = $("#quantity" + productId);
+    var quantityInput = $('#quantity' + productId);
     var currentQuantity = parseInt(quantityInput.val(), 10);
 
     if (currentQuantity > 1) {
@@ -20,7 +22,7 @@ function decreaseQuantity(productId) {
 
 // Example function to increase quantity
 function increaseQuantity(productId) {
-    var quantityInput = $("#quantity" + productId);
+    var quantityInput = $('#quantity' + productId);
     var currentQuantity = parseInt(quantityInput.val(), 10);
 
     // Increase the quantity by 1
@@ -31,33 +33,34 @@ function increaseQuantity(productId) {
     // Implement any additional logic you need, such as updating the server or recalculating prices
 }
 
+
 // Retrieve Cart Items AJAX request
 function updateCartItemsList() {
     $.ajax({
-        type: "GET",
-        url: "/api/cart/getCartItemsApi",
+        type: 'GET',
+        url: '/api/cart/getCartItemsApi',
         headers: {
-            "guest-token": getGuestToken(),
+            'guest-token': getGuestToken(),
         },
-        success: function (response) {
+        success: function(response) {
             console.log(response.cartItems);
             displayCartItems(response.cartItems);
             // alert(response.cartItems.length);
-            jQuery(".nav-item .cart-count").text(response.cartItems.length);
+            jQuery('.nav-item .cart-count').text(response.cartItems.length);
         },
-        error: function (error) {
-            console.error("Error retrieving cart items", error);
-        },
+        error: function(error) {
+            console.error('Error retrieving cart items', error);
+        }
     });
 }
 
 // Function to get or generate guest token
 function getGuestToken() {
-    var guestToken = localStorage.getItem("guestToken");
+    var guestToken = localStorage.getItem('guestToken');
     console.log(guestToken);
     if (!guestToken) {
         guestToken = generateGuestToken();
-        localStorage.setItem("guestToken", guestToken);
+        localStorage.setItem('guestToken', guestToken);
     }
     return guestToken;
 }
@@ -67,70 +70,44 @@ function generateGuestToken() {
     return Math.random().toString(36).substring(2) + Date.now().toString(36);
 }
 
+
 // Display Cart Items in the right sidebar
 function displayCartItems(cartItems) {
     console.log(cartItems);
-    var baseUrl = jQuery("#baseUrl").val();
-    var cartItemsList = $("#cart-items-list");
+    var baseUrl = jQuery('#baseUrl').val();
+    var cartItemsList = $('#cart-items-list');
     cartItemsList.empty();
 
-    cartItems.forEach(function (item, index) {
+    cartItems.forEach(function(item, index) {
         // Create a new product element for each cart item
         var productElement = $(' <div class="products row">');
 
         // <img src="{{ asset("storage/") }}' + '/' + item.imageFile + '" alt="Image" style="height:80px;">
 
         // Product image (replace 'item.product_image_url' with the actual property in your response)
-        productElement.append(
-            '<img src="' +
-                baseUrl +
-                "/storage/" +
-                item.product_image +
-                '" alt="" srcset="">'
-        );
+        productElement.append('<img src="'+ baseUrl + '/storage/' + item.product_image + '" alt="" srcset="">');
 
         // Product details
         var productDetails = $('<div class="col">');
-        productDetails.append(
-            '<a href="#" class="product-name"><p>' +
-                item.product_name +
-                "</p></a> "
-        );
+        productDetails.append('<a href="#" class="product-name"><p>' + item.product_name + '</p></a> ');
 
         // Price display (replace 'item.total_price' with the actual property in your response)
-        productDetails.append(
-            '<p class="price"><span id="totalPriceDisplay' +
-                index +
-                '">' +
-                item.Preis_zzgl_MwSt +
-                "</span>€</p> "
-        );
+        productDetails.append('<p class="price"><span id="totalPriceDisplay' + index + '">' + item.Preis_zzgl_MwSt +
+            '</span>€</p> ');
 
         // Quantity controls
         var quantityControls = $('<div class="sidebar-quantity">');
-        quantityControls.append(
-            '<button class="quantity-btn" onclick="decreaseQuantity(' +
-                item.product_id +
-                ')"><i class="fa-solid fa-minus"></i></button>'
-        );
-        quantityControls.append(
-            '<input type="text" id="quantity' +
-                item.product_id +
-                '" value="' +
-                item.quantity +
-                '" min="1" readonly />'
-        );
-        quantityControls.append(
-            '<button class="quantity-btn" onclick="increaseQuantity(' +
-                item.product_id +
-                ')"><i class="fa-solid fa-plus"></i></button></div></div></div>'
+        quantityControls.append('<button class="quantity-btn" onclick="decreaseQuantity(' + item.product_id +
+            ')"><i class="fa-solid fa-minus"></i></button>');
+        quantityControls.append('<input type="text" id="quantity' + item.product_id + '" value="' + item.quantity +
+            '" min="1" readonly />');
+        quantityControls.append('<button class="quantity-btn" onclick="increaseQuantity(' + item.product_id +
+            ')"><i class="fa-solid fa-plus"></i></button></div></div></div>'
         );
 
-        quantityControls.append(
-            '<i style="margin-left:2.5rem; font-size:23px;" class="fa-solid fa-circle-xmark" onclick="removeCartItem(' +
-                item.product_id +
-                ')"></i></div></div></div>'
-        );
+        quantityControls.append('<i style="margin-left:2.5rem; font-size:23px;" class="fa-solid fa-circle-xmark" onclick="removeCartItem(' +
+            item.product_id + ')"></i></div></div></div>');
+
 
         productDetails.append(quantityControls);
         productElement.append(productDetails);
@@ -140,16 +117,35 @@ function displayCartItems(cartItems) {
 
     // Display grand total
     var grandTotal = calculateGrandTotal(cartItems);
-    $("#grandTotalDisplay").text(grandTotal + "€");
+    $('#grandTotalDisplay').text(grandTotal + '€');
 }
 
-// Calculate grand total function
-function calculateGrandTotal(cartItems) {
-    var grandTotal = 0;
 
-    cartItems.forEach(function (item) {
-        grandTotal += item.Preis_zzgl_MwSt * item.quantity;
-    });
+//  START CALCULATION
 
-    return grandTotal;
-}
+    // Calculate grand total function
+    function calculateGrandTotal(cartItems) {
+        var grandTotal = 0;
+        cartItems.forEach(function(item) {
+            grandTotal += item.Preis_zzgl_MwSt * item.quantity;
+        });
+        return grandTotal.toFixed(2);
+    }
+
+    // Function for Calculate total tax 19%
+    function calculateTaxTotal(cartItems) {
+        var allSubTotal = calculateGrandTotal(cartItems);
+        var taxTotal = ((allSubTotal*19)/100);
+        return taxTotal.toFixed(2);
+    }
+
+    // Function for Calculate total summary (Order Total =(Subtotal+Shipping+Tax))
+    function calculateOrderSummaryTotal(cartItems) {
+        var allSubTotal = parseFloat(calculateGrandTotal(cartItems));
+        var allTaxTotal = parseFloat(calculateTaxTotal(cartItems));
+        var shipping    = parseFloat('21');
+        var orderSummaryTotal    = allSubTotal+shipping+allTaxTotal;
+        return orderSummaryTotal.toFixed(2);
+    }
+
+//  END CALCULATION
