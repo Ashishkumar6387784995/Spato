97de2ab3ffba20cae8c09004fb092747b255f374
bhaskar <bhaskarpratap111@gmail.com>
2024-03-06 13:24:40 +0530

    dd

--- resources/views/admin_theme/pages/assignments/addAssignments.blade.php
+++ resources/views/admin_theme/pages/assignments/addAssignments.blade.php
@@ -682,54 +682,105 @@
   <!-- container-scroller -->
   <!-- plugins:js -->
 
-
-
   <script>
-  document.addEventListener('DOMContentLoaded', function() {
-   setupInputListeners1();
+    // function for find Generated No
+      function findGeneratedProductDtl(page_name){
 
-   function setupInputListeners1() {
-    document.querySelectorAll(
-     'input[id^="Quantity_"], input[id^="Rate_"], input[id^="Discount_"], input[id^="Amount_"]').forEach(function(
-     input) {
-     input.addEventListener('input', function() {
-      var idParts = this.id.split('_');
-      var index = idParts[1];
+      // alert(page_name);
+      var baseUrl = window.location.origin;
+      var generatedNo = jQuery('#generatedNo').val();
+      // console.log(generatedNo);
 
-      var quantity = parseFloat(document.getElementById('Quantity_' + index).value) || 0;
-      var rate = parseFloat(document.getElementById('Rate_' + index).value) || 0;
-      var discountPercentage = parseFloat(document.getElementById('Discount_' + index).value) || 0;
+      var generatedNoList = jQuery('#generatedNoList ul');
+      var tableTbody = jQuery('#table tbody');
+      tableTbody.find('.weNeedRemove').empty();  // make empty listing
+      generatedNoList.empty();
+      if (generatedNo=='') {
+        return false;       // stop if generatedNo is null
+      }
 
-      var discountAmount = (rate * discountPercentage) / 100;
-      var discountedRate = rate - discountAmount;
+      // Make a GET request using AJAX
+      $.ajax({
+        url: '/api/getGeneratedProductDtlApi', // Replace with the actual endpoint URL
+        method: 'GET',
+        data: {page_name:page_name, generatedNo: generatedNo},
+        success: function(response) {
+          // Handle the successful response
+          console.log('Response :', response);
+          if (response.success) {
+            console.log(response.productsList);
 
-      var amount = quantity * discountedRate;
-      document.getElementById('Amount_' + index).value = amount.toFixed(2);
+            // for Angebots_Nr list
+            $.each(response.productsList, function(index, item) {
+              // Create a new product element for each cart item
+              generatedNoList.append(`
+                <li class="liGenerateNoDtl slide-in-blurred-top" generatedNo="${item.Angebots_Nr}">
+                  ${item.Angebots_Nr}
+                </li>
+              `);
+            });
 
-      // Recalculate subtotal after each input change
-      calculateSubTotal();
-     });
-    });
+            // show product in table
+            if (response.status) {
+              $.each(response.success, function(i, tableItem) {
+                // Create a new product element for each cart tableItem
+                tableTbody.append(`
+                  <tr class="weNeedRemove">
+                    <td>
+                      <input type="text" value='${++i}' name='inputs[${i}][POS]' placeholder='#' />
+                    </td>
 
-    // Add event listeners for Adjustment, SGST, and CGST fields
-    document.getElementById('Adjustment').addEventListener('input', function() {
-     calculateSubTotal();
-    });
+                    <td>
+                      <input type="text" value='${tableItem.Produkt}' name='inputs[${i}][Produkt]' id="Produkt_${i}" onkeyup="getProductDetailsDRP('${i}');" placeholder="#"/>
+                      <br>
+                      <div class="guessProductDtl">
+                        <ul>
 
-    document.getElementById('SGST').addEventListener('input', function() {
-     calculateSubTotal();
-    });
+                        </ul>
+                      </div>
+                    </td>
 
-    document.getElementById('CGST').addEventListener('input', function() {
-     calculateSubTotal();
-    });
+                    <td>
+                      <input type="text" value='${tableItem.Beschreibung}' name='inputs[${i}][Beschreibung]' id="Beschreibung_${i}" placeholder="#"/>
+                    </td>
 
-    document.getElementById('IGST').addEventListener('input', function() {
-     calculateSubTotal();
-    });
-   }
+                    <td>
+                      <input type="text" value='${tableItem.Menge}' name='inputs[${i}][Menge]' placeholder="#"/>
+                    </td>
 
-   var i = 0;
+                    <td>
+                      <input type="text" value='${tableItem.Einheit}' name='inputs[${i}][Einheit]' id="Quantity_${i}" placeholder='#'  onclick="handleClick('Quantity_${i}')"/>
+                    </td>
+
+                    <td>
+                      <input type="text" value='${tableItem.Einzelpreis}' name='inputs[${i}][Einzelpreis]' id="Rate_${i}" placeholder='#'  onclick="handleClick('Rate_${i}')"/>
+                    </td>
+                    
+                    <td>
+                      <input type="text" value='${tableItem.Rabatt}' name='inputs[${i}][Rabatt]' id="Discount_${i}" placeholder='#'  onclick="handleClick('Discount_${i}')" style="width:30px;"/><span>% C2</span>
+                    </td>
+
+                    <td>
+                      <input type="text" value='${tableItem.Gesamtpreis}' name='inputs[${i}][Gesamtpreis]' id="Amount_${i}" placeholder='#' />
+                    </td>
+
+                    <td>
+                      <button class="remove-table-row  btn btn-sm">Delete</button>
+                    </td>
+                  </tr>
+                `);
+              
+                window.i= i;
+
+              });
+              generatedNoList.empty();
+              
+            }
+         
+          }
+          console.log('i', + i);
+
+          // var i = 0;
    var No = 1;
 
    document.getElementById('add').addEventListener('click', function() {
@@ -781,6 +832,71 @@
     // Recalculate subtotal after adding a new row
     calculateSubTotal();
    });
+        },
+        error: function(error) {
+          // Handle errors
+          console.error('Error:', error);
+          tableTbody.append('<tr class="weNeedRemove"><th  colspan="8" style="text-align:center;">Sorry! we are facing some internal errors.</th></tr>');
+        }
+      });
+    }
+
+
+    // function for set li test in companyName input feild
+    $(document).on('click', '.liGenerateNoDtl', function() {
+      var clickedGeneratedNo = jQuery(this).attr('generatedNo');
+      jQuery('#generatedNo').val(clickedGeneratedNo).trigger("keyup");
+      jQuery('#generatedNoList ul').empty();
+    });
+  </script>
+
+
+  <script>
+  document.addEventListener('DOMContentLoaded', function() {
+   setupInputListeners1();
+
+   function setupInputListeners1() {
+    document.querySelectorAll(
+     'input[id^="Quantity_"], input[id^="Rate_"], input[id^="Discount_"], input[id^="Amount_"]').forEach(function(
+     input) {
+     input.addEventListener('input', function() {
+      var idParts = this.id.split('_');
+      var index = idParts[1];
+
+      var quantity = parseFloat(document.getElementById('Quantity_' + index).value) || 0;
+      var rate = parseFloat(document.getElementById('Rate_' + index).value) || 0;
+      var discountPercentage = parseFloat(document.getElementById('Discount_' + index).value) || 0;
+
+      var discountAmount = (rate * discountPercentage) / 100;
+      var discountedRate = rate - discountAmount;
+
+      var amount = quantity * discountedRate;
+      document.getElementById('Amount_' + index).value = amount.toFixed(2);
+
+      // Recalculate subtotal after each input change
+      calculateSubTotal();
+     });
+    });
+
+    // Add event listeners for Adjustment, SGST, and CGST fields
+    document.getElementById('Adjustment').addEventListener('input', function() {
+     calculateSubTotal();
+    });
+
+    document.getElementById('SGST').addEventListener('input', function() {
+     calculateSubTotal();
+    });
+
+    document.getElementById('CGST').addEventListener('input', function() {
+     calculateSubTotal();
+    });
+
+    document.getElementById('IGST').addEventListener('input', function() {
+     calculateSubTotal();
+    });
+   }
+
+   
 
    document.addEventListener('click', function(event) {
     if (event.target.classList.contains('remove-table-row')) {
@@ -1011,114 +1127,7 @@
 </script>
 
 
-  <script>
-    // function for find Generated No
-      function findGeneratedProductDtl(page_name){
-
-      // alert(page_name);
-      var baseUrl = window.location.origin;
-      var generatedNo = jQuery('#generatedNo').val();
-      // console.log(generatedNo);
-
-      var generatedNoList = jQuery('#generatedNoList ul');
-      var tableTbody = jQuery('#table tbody');
-      tableTbody.find('.weNeedRemove').empty();  // make empty listing
-      generatedNoList.empty();
-      if (generatedNo=='') {
-        return false;       // stop if generatedNo is null
-      }
-
-      // Make a GET request using AJAX
-      $.ajax({
-        url: '/api/getGeneratedProductDtlApi', // Replace with the actual endpoint URL
-        method: 'GET',
-        data: {page_name:page_name, generatedNo: generatedNo},
-        success: function(response) {
-          // Handle the successful response
-          console.log('Response :', response);
-          if (response.success) {
-            console.log(response.productsList);
-
-            // for Angebots_Nr list
-            $.each(response.productsList, function(index, item) {
-              // Create a new product element for each cart item
-              generatedNoList.append(`
-                <li class="liGenerateNoDtl slide-in-blurred-top" generatedNo="${item.Angebots_Nr}">
-                  ${item.Angebots_Nr}
-                </li>
-              `);
-            });
-
-            // show product in table
-            if (response.status) {
-              $.each(response.success, function(i, tableItem) {
-                // Create a new product element for each cart tableItem
-                tableTbody.append(`
-                  <tr class="weNeedRemove">
-                    <td>
-                      <input type="text" value='${++i}' name='inputs[${i}][POS]' placeholder='#' />
-                    </td>
-
-                    <td>
-                      <input type="text" value='${tableItem.Produkt}' name='inputs[${i}][Produkt]' id="Produkt_${i}" onkeyup="getProductDetailsDRP('${i}');" placeholder="#"/>
-                      <br>
-                      <div class="guessProductDtl">
-                        <ul>
-
-                        </ul>
-                      </div>
-                    </td>
-
-                    <td>
-                      <input type="text" value='${tableItem.Beschreibung}' name='inputs[${i}][Beschreibung]' id="Beschreibung_${i}" placeholder="#"/>
-                    </td>
-
-                    <td>
-                      <input type="text" value='${tableItem.Menge}' name='inputs[${i}][Menge]' placeholder="#"/>
-                    </td>
-
-                    <td>
-                      <input type="text" value='${tableItem.Einheit}' name='inputs[${i}][Einheit]' id="Quantity_${i}" placeholder='#'  onclick="handleClick('Quantity_${i}')"/>
-                    </td>
-
-                    <td>
-                      <input type="text" value='${tableItem.Einzelpreis}' name='inputs[${i}][Einzelpreis]' id="Rate_${i}" placeholder='#'  onclick="handleClick('Rate_${i}')"/>
-                    </td>
-                    
-                    <td>
-                      <input type="text" value='${tableItem.Rabatt}' name='inputs[${i}][Rabatt]' id="Discount_${i}" placeholder='#'  onclick="handleClick('Discount_${i}')" style="width:30px;"/><span>% C2</span>
-                    </td>
-
-                    <td>
-                      <input type="text" value='${tableItem.Gesamtpreis}' name='inputs[${i}][Gesamtpreis]' id="Amount_${i}" placeholder='#' />
-                    </td>
-
-                    <td>
-                      <button class="remove-table-row  btn btn-sm">Delete</button>
-                    </td>
-                  </tr>
-                `);
-              });
-              generatedNoList.empty();
-            }
-          }
-        },
-        error: function(error) {
-          // Handle errors
-          console.error('Error:', error);
-          tableTbody.append('<tr class="weNeedRemove"><th  colspan="8" style="text-align:center;">Sorry! we are facing some internal errors.</th></tr>');
-        }
-      });
-    }
-
-    // function for set li test in companyName input feild
-    $(document).on('click', '.liGenerateNoDtl', function() {
-      var clickedGeneratedNo = jQuery(this).attr('generatedNo');
-      jQuery('#generatedNo').val(clickedGeneratedNo).trigger("keyup");
-      jQuery('#generatedNoList ul').empty();
-    });
-  </script>
-
+  
 
 
 
